d94ff36325b6a1a4b28334b13eaa24953a760c70
Ikhmal <Ikhmal1410@gmail.com>
2024-12-20 15:53:05 +0800

    test

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB4bjdKKk1qcMY4qW75cltCkHXTkeevtuw",
-  authDomain: "mvp-clinic-qms-webapp.firebaseapp.com",
-  projectId: "mvp-clinic-qms-webapp",
-  storageBucket: "mvp-clinic-qms-webapp.firebasestorage.app",
-  messagingSenderId: "13443185885",
-  appId: "1:13443185885:web:7b9082fb8e3f5c91b96f34",
-  measurementId: "G-BWE50YG4NK"
+  apiKey: "AIzaSyC95vaM_RJJYB_QMigVxjA3fJDIqcWu4wc",
+  authDomain: "testing-f5721.firebaseapp.com",
+  projectId: "testing-f5721",
+  storageBucket: "testing-f5721.firebasestorage.app",
+  messagingSenderId: "517271989421",
+  appId: "1:517271989421:web:ed038902c20c774e68ea24",
+  measurementId: "G-CSVH44X4PR"
 };
 
 
